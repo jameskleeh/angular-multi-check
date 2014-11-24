1e6fcc7e834195c8ba405876b30b868be1399b6a
James Kleeh <jjkleeh@ysu.edu>
2014-11-24 14:35:20 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+angular.module('myApp', ['angular-multi-check'])
+  .controller('MyController', function($scope) {
+    $scope.people = [
+      {id: 0, name: 'Joe'},
+      {id: 1, name: 'John'},
+      {id: 2, name: 'Jim'},
+      {id: 3, name: 'Jason'},
+      {id: 4, name: 'Jack'},
+      {id: 5, name: 'Jamiroquai'},
+      {id: 6, name: 'Jacob'},
+      {id: 7, name: 'Jared'},
+      {id: 8, name: 'Jeff'},
+      {id: 9, name: 'Josh'}
+    ];
+  });
